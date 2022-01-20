c8170d97eb89854e8984659121e7c367f1b50793
ashikulashi <ashismart554@gmail.com>
2022-01-21 01:29:00 +0530

    rajasekar done his code

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,11 @@ package org.sample;
 
 public class Sample {
 	
+	private void m3() {
+		System.out.println("rajesekar code");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Done");
